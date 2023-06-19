7449bd8325adf37974ffc44e1f69b60c4788c64a
artearc <alexruicha@gmail.com>
2023-06-19 13:58:17 +0200

    calculadora

--- prueba.go
+++ prueba.go
@@ -3,15 +3,29 @@ package main
 import "fmt"
 
 func main() {
-	var numeroUno int
-	var numeroDos int
+	var numeroUno float64
+	var numeroDos float64
 	fmt.Println("Introduce el primer número")
 	fmt.Scan(&numeroUno)
 	fmt.Println("Introduce el segundo número")
 	fmt.Scan(&numeroDos)
 	fmt.Println(SumarNumeros(numeroUno, numeroDos))
+	fmt.Println("Introduce el primer número")
+	fmt.Scan(&numeroUno)
+	fmt.Println("Introduce el segundo número")
+	fmt.Scan(&numeroDos)
+	fmt.Println(RestarNumerosNaturales(numeroUno, numeroDos))
 }
 
-func SumarNumeros(n1 int, n2 int) int {
+func SumarNumeros(n1 float64, n2 float64) float64 {
 	return n1 + n2
+}
+func RestarNumerosNaturales(n1 float64, n2 float64) float64 {
+	var resultado float64
+	if n1 < n2 {
+		fmt.Println("el resultado será negativo")
+	} else {
+		resultado = n1 - n2
+	}
+	return resultado
 }
